af2d0c86c81ab3db3419d11744a58f02a49622ef
aioros2016 <d_iii@aliyun.com>
2019-06-26 15:41:45 +0800

    测试提交

--- src/helps/util.ts
+++ src/helps/util.ts
@@ -5,5 +5,5 @@ export function isDate(val: any): val is Date {
 }
 
 export function isObject(val: any): val is Object {
-  return val !== null && typeof val === 'object'
+  return val !== null && typeof val === 'object';
 }
